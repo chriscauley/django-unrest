c1946bed867a3965abfc5e32a97044748eabd3b9
chriscauley <chris@lablackey.com>
2019-01-03 03:48:28 +0000

    ready for first version

--- setup.py
+++ setup.py
@@ -4,15 +4,15 @@ with open("README.md", "r") as fh:
   long_description = fh.read()
 
 setup(
-  name = 'lablackey',
-  packages = ['lablackey','lablackey.db'],
-  version = '0.1.6',
+  name = 'django-unrest',
+  packages = ['unrest'],
+  version = '0.0.1',
   description = 'A collection of tools for django',
   long_description=long_description,
   long_description_content_type="text/markdown",
   author = 'Chris Cauley',
   author_email = 'chris@lablackey.com',
-  url = 'https://github.com/chriscauley/lablackey',
+  url = 'https://github.com/chriscauley/django-unrest',
   keywords = ['utils','django'],
   license = 'GPL',
   include_package_data = True,
