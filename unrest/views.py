a0a759198e05db98751d52acc6158995158d2cae
chriscauley <chris@lablackey.com>
2019-07-01 10:55:17 +0000

    ading index and superuser api view

--- unrest/views.py
+++ unrest/views.py
@@ -1,9 +1,38 @@
 from django.apps import apps
-from django.http import JsonResponse, HttpResponseRedirect
+from django.views.decorators.csrf import ensure_csrf_cookie
+from django.http import JsonResponse, HttpResponseRedirect, HttpResponse
 import json
+import subprocess
+
+@ensure_csrf_cookie
+def index(request, path='dist/index.html'):
+    f = open(path,'r')
+    response = HttpResponse(f.read())
+    _hash = subprocess.check_output(['git', 'rev-parse', 'HEAD'])
+    response.set_cookie("GIT_HASH", _hash.decode('utf-8').strip())
+    return response
 
 def redirect(request,url=None):
     return HttpResponseRedirect(url)
+
+def superuser_api_view(request,app_name,model_name):
+    app = apps.get_app_config(app_name)
+    model = app.get_model(model_name)
+    data = json.loads(request.body.decode('utf-8') or "{}")
+    if not request.method == "POST":
+        return list_view(request, app_name, model_name)
+    if not request.user.is_superuser:
+        raise NotImplementedError("Only superusers can use this view")
+    data = json.loads(request.body.decode('utf-8') or "{}")
+    id = data.pop("id", 0)
+    if id:
+        obj = model.objects.get(id=id)
+        obj.data = data
+        obj.save()
+    else:
+        obj = model.objects.create(data=data)
+    return JsonResponse(obj.as_json)
+
 
 def list_view(request,app_name,model_name):
     app = apps.get_app_config(app_name)
