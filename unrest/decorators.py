b8d9944effc554e396c0471889f74b3014a48933
chriscauley <chris@lablackey.com>
2020-05-01 15:22:04 +0000

    cache wrappers

--- unrest/decorators.py
+++ unrest/decorators.py
@@ -10,3 +10,19 @@ def login_required(func):
       return func(request, *args, **kwargs)
     return JsonResponse({}, status_code=403)
   return wrapped
+
+def cached_method(target):
+  def wrapper(*args, **kwargs):
+    obj = args[0]
+    name = '___' + target.__name__
+
+    if not hasattr(obj, name):
+      value = target(*args, **kwargs)
+      setattr(obj, name, value)
+
+    return getattr(obj, name)
+
+  return wrapper
+
+def cached_property(target):
+  return property(cached_method(target))
