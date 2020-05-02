e1ac9a14fc288971bb7ced8dff3fa9459d1dd3df
chriscauley <chris@lablackey.com>
2020-05-02 19:18:40 +0000

    moving some urls here from noticed

--- unrest/urls.py
+++ unrest/urls.py
@@ -1,7 +1,21 @@
-from django.urls import path
+from django.conf import settings
+from django.urls import path, re_path, include
 
-from unrest import views
+from unrest.views import spa, favicon
 
 urlpatterns = [
-  path('favicon.ico', views.favicon)
+    path('favicon.ico', favicon),
+    re_path('^(?:login|logout|signup|reset-password|new)/', spa),
+    re_path('^$', spa),
+    re_path('', include('unrest.user.urls')),
+    re_path('', include('unrest.schema.urls')),
 ]
+
+if settings.DEBUG:  # pragma: no cover
+    from django.views.static import serve
+    urlpatterns += [
+        re_path(r'^media/(?P<path>.*)$', serve, {
+            'document_root': settings.MEDIA_ROOT,
+            'show_indexes': True
+        }),
+    ]
