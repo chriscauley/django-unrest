b205847348bf8ac7b4ac68b6f662dec8b0187012
chriscauley <chris@lablackey.com>
2020-05-02 20:43:10 +0000

    rainbow favicons

---
+++ unrest/static/_logos.sh
@@ -0,0 +1,14 @@
+convert logo-white-iris.png favicon.ico
+convert logo-red-iris.png ur-f00.png
+convert logo-red-iris.png -modulate 100,100,16.666 ur-08f.png
+convert logo-red-iris.png -modulate 100,100,33.333 ur-00f.png
+convert logo-red-iris.png -modulate 100,100,50.000 ur-80f.png
+convert logo-red-iris.png -modulate 100,100,66.666 ur-f0f.png
+convert logo-red-iris.png -modulate 100,100,83.333 ur-f08.png
+
+convert logo-cyan-iris.png ur-0ff.png
+convert logo-cyan-iris.png -modulate 100,100,16.666 ur-f80.png
+convert logo-cyan-iris.png -modulate 100,100,33.333 ur-ff0.png
+convert logo-cyan-iris.png -modulate 100,100,50.000 ur-8f0.png
+convert logo-cyan-iris.png -modulate 100,100,66.666 ur-0f0.png
+convert logo-cyan-iris.png -modulate 100,100,83.333 ur-0f8.png
