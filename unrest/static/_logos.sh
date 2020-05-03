8909da7320c157cfe24435dc7c8bd018d0db7857
chriscauley <chris@lablackey.com>
2020-05-03 15:26:46 +0000

    adding shadows to favicon

--- unrest/static/_logos.sh
+++ unrest/static/_logos.sh
@@ -12,3 +12,9 @@ convert logo-cyan-iris.png -modulate 100,100,33.333 ur-ff0.png
 convert logo-cyan-iris.png -modulate 100,100,50.000 ur-8f0.png
 convert logo-cyan-iris.png -modulate 100,100,66.666 ur-0f0.png
 convert logo-cyan-iris.png -modulate 100,100,83.333 ur-0f8.png
+
+for i in favicon.ico `ls ur-*.png`;
+do
+    convert $i \( +clone  -background white  -shadow 80x1+0+0 \) \
+            +swap -background none -layers merge  +repage $i
+done
