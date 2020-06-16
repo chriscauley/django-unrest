36de92cefec6c8951afee5caf76befbbab7b65b2
chriscauley <chris@lablackey.com>
2020-06-16 11:36:15 +0000

    adding user_json.extras as an alternaltive to get_extra

--- unrest/user/views.py
+++ unrest/user/views.py
@@ -13,7 +13,11 @@ def user_json(request):
     data = { k: getattr(user,k) for k in keys }
     if hasattr(user_json, 'get_extra'):
         data.update(user_json.get_extra(user))
+    for func in user_json.extras:
+        data.update(func(request))
     return JsonResponse({ 'user': data })
+
+user_json.extras = []
 
 def logout_ajax(request):
   logout(request)
