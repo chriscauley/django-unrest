027c8312c0716e1fa4cca44c988ede2e4e199450
chriscauley <chris@lablackey.com>
2021-09-05 13:31:07 +0000

    default value for per_page

--- unrest/pagination.py
+++ unrest/pagination.py
@@ -4,6 +4,7 @@ noop = lambda i: i
 
 def paginate(items, per_page=10, process=noop, extra={}, query_dict={}):
     page = int(query_dict.get('page') or 1)
+    per_page = int(query_dict.get('per_page') or 0) or per_page
     offset = (page - 1) * per_page
     total = len(items)
     return {
